a3df58256ae98853d8d520e39696dae9d5912770
maheuma14 <umaa9310@gmail.com>
2021-09-14 09:53:10 +0530

    task completed

---
+++ src/org/itc/Single.java
@@ -0,0 +1,24 @@
+package org.itc;
+
+public class Single {
+	private void companyName() {
+		System.out.println("Compamny Name is Delta");
+		}
+private void comapnyId() {
+	System.out.println("Company Id is 98383399");
+
+
+}	
+private void companyAddress() {
+	System.out.println("company Address iis Trichy");
+
+}
+public static void main(String[] args) {
+   Single s =new Single();
+   s.companyName();
+   s.comapnyId(); 
+   s.companyAddress();
+   
+}
+
+}
